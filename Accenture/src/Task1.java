ae5b2368ef842f3ec4cd09b385dcaf1f7961eb99
jolantalei <jolanta.leigute@gmail.com>
2022-06-28 11:52:53 +0300

    added all new files

---
+++ Accenture/src/Task1.java
@@ -0,0 +1,23 @@
+public class Task1 {
+
+    public static void main(String[] args) {
+        byte byteVar = 1;
+        short shortVar = 1;
+        int intVar = 1;
+        long longVar = 1;
+        float floatVar = 1.2f;
+        double doubleVar = 1.2;
+        boolean isTrue = true;
+        char charVar = 'a';
+
+        System.out.println(byteVar);
+        System.out.println(shortVar);
+        System.out.println(intVar);
+        System.out.println(longVar);
+        System.out.println(floatVar);
+        System.out.println(doubleVar);
+        System.out.println(isTrue);
+        System.out.println(charVar);
+
+    }
+}
