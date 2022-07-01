f64a89ce3d32e034081a23139a3f637c02774532
jolantalei <jolanta.leigute@gmail.com>
2022-07-01 09:39:37 +0300

    day4 tasks fixed

--- Bootcamp/src/Tasks4/ExtraTasks.java
+++ Bootcamp/src/Tasks4/ExtraTasks.java
@@ -1,13 +1,13 @@
 package Tasks4;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Iterator;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
 public class ExtraTasks {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // 1. declare and initialize String array with 7 arbitrary colors.
 // 2. Create new list "colors" (ArrayList) of type String
 // 3. Copy/add all elements from String array to colors list
@@ -34,6 +34,17 @@ public class ExtraTasks {
         for (String col : arrayListColors){
             System.out.println(col);
         }
+        List<String> arrayListColor2 = arrayListColors.subList(2,5);
+        System.out.println(arrayListColor2);
+        // Write a program which counts how many unique words are in text (see class Text, variable TEXT as a comment here)
+        //Think of how you could use any of the Set implementation to implement
+        //this program.
+
+        // Bonus:
+        // Ignore case sensitivity. For example, Apple and apple should be counted as the same word.
+        //Hint: to get the text as an String array can be regular expression used e.g.: String[] words = Text.TEXT.split("\\s+");
+        FileWriter myWriter = new FileWriter("C:\\Users\\jolan\\IdeaProjects\\Bootcamp\\src\\Tasks4\\TestFile.txt");
+        myWriter.close();
 
 
 
