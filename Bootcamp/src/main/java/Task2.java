65fe15e4a461e9614497a9175ec8af07bf5711a9
jolantalei <jolanta.leigute@gmail.com>
2022-07-03 23:16:20 +0300

    JUnit task

--- Bootcamp/src/main/java/Task2.java
+++ Bootcamp/src/main/java/Task2.java
@@ -3,63 +3,61 @@ import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
 
-        compareNumbers();
-        compareBoolean();
-        wholeNumber();
-        oddNumber();
-        changeBoolean();
-        numberConditions();
-
-    }
-
-    static void compareNumbers () {
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter value a: ");
         int a = scanner.nextInt();
         System.out.println("Enter value b: ");
         int b = scanner.nextInt();
-
-        boolean a1 = true;
-        boolean b1 = false;
-
-        System.out.println("is " + a+ " equal to " + b +"? -" + ((a == b) ? a1 : b1));
-        System.out.println("is " + a+ " less than " + b +"? -" + ((a < b) ? a1 : b1));
-        System.out.println("is " + a+ " less or equal to " + b +"? -" + ((a <= b) ? a1 : b1));
-        System.out.println("is " + a+ " greater than " + b +"? -" + ((a > b) ? a1 : b1));
-        System.out.println("is " + a+ " greater or equal to " + b +"? -" + ((a >= b) ? a1 : b1));
-    }
-
-    static void compareBoolean() {
-
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter b1: ");
         boolean b1 = scanner.nextBoolean();
         System.out.println("Enter b2 ");
         boolean b2 = scanner.nextBoolean();
-        System.out.println("Is b1 and b2 equal? -" + (b1 == b2));
-    }
-
-    static void wholeNumber() {
-        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Is b1 and b2 equal? -" + (b1 == b2));
         System.out.println("Enter number: ");
         int inputNumber = scanner.nextInt();
-        System.out.println(inputNumber % 2 == 0);
-    }
-
-    static void oddNumber() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter number: ");
-        int inputNumber = scanner.nextInt();
-        System.out.println(inputNumber % 2 != 0);
-    }
+        int inputNumber2 = scanner.nextInt();
 
-    static void changeBoolean() {
-        Scanner scanner = new Scanner(System.in);
+        compareNumbers(a, b);
+        compareBoolean(b1, b2);
         System.out.println("Enter boolean value: ");
-        boolean b1 = scanner.nextBoolean();
-        System.out.println("Opposite of " + b1 + " is " + !b1);
+        boolean b3 = scanner.nextBoolean();
+        System.out.println(wholeNumber(inputNumber));
+        System.out.println(oddNumber(inputNumber2));
+        changeBoolean(b3);
+        numberConditions();
 
+    }
+
+    static String compareNumbers(int a, int b) {
+        if (a == b) {
+            return "is " + a + " equal to " + b + "? -" + true;
+        } else if (a <= b) {
+            return "is " + a + " less than " + b + "? -" + true;
+        } else if (a < b) {
+            return "is " + a + " less or equal to " + b + "? -" + true;
+        } else if (a > b) {
+            return "is " + a + " greater or equal to " + b + "? -" + true;
+        }else{
+            return "false";
+        }
+    }
+
+    static boolean compareBoolean(boolean b1, boolean b2) {
+        return b1 == b2;
+    }
+
+    static boolean wholeNumber(int inputNumber) {
+        return inputNumber % 2 == 0;
+    }
+
+    static boolean oddNumber(int inputNumber) {
+        return inputNumber % 2 != 0;
+    }
+
+    static boolean changeBoolean(boolean b1) {
+        boolean b2=false;
+        return b1!=b2;
     }
 
     static void numberConditions() {
@@ -79,10 +77,10 @@ public class Task2 {
         }
     }
 
-    public static int sum (int a, int b){
+    public static int sum(int a, int b) {
         a = 10;
-        b= 11;
-        int sum = a+ b;
+        b = 11;
+        int sum = a + b;
         return sum;
     }
 
--- Bootcamp/src/test/java/Task2Test.java
+++ Bootcamp/src/test/java/Task2Test.java
@@ -6,28 +6,34 @@ class Task2Test {
 
     @Test
     void compareNumbers() {
+        assertEquals(true, Task2.compareNumbers(2,2));
 
     }
 
     @Test
     void compareBoolean() {
+        assertEquals(true,Task2.compareBoolean(true,true));
     }
 
     @Test
     void wholeNumber() {
+        assertEquals(true, Task2.wholeNumber(4));
 
     }
 
     @Test
     void oddNumber() {
+        assertEquals(true, Task2.oddNumber(5));
     }
 
     @Test
     void changeBoolean() {
+        assertEquals(true, Task2.changeBoolean(true));
     }
 
     @Test
     void numberConditions() {
+
     }
 
 
